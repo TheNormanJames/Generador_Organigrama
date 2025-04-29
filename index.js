bf97dc7070e84cc9830c41a91b85fc3921bdd622
carlosbustos <carlosbustosc09@gmail.com>
2025-04-29 08:58:01 -0500

    solucion CORS

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ const usarExpress = express();
 
 
 //----------configuracion de datos-------------//
-usarExpress.use( cors() );
+//usarExpress.use( cors() );
+
+app.use(cors({
+  origin: 'https://www.eltiempo.com', // o '*' para todos los orígenes (no recomendado en producción)
+}));
+
+app.post('/chat', (req, res) => {
+  res.json({ message: 'CORS habilitado correctamente' });
+});
+
 usarExpress.use( express.json() );
 usarExpress.use( express.urlencoded( { extended:true }  ))
 
